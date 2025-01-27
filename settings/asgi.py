8b70f0fcbddb549093a82e51dd00c2235bdb5661
0xjuju <jermol.jupiter@gmail.com>
2025-01-27 17:12:17 -0500

    staticfiles and heroku setup

--- settings/asgi.py
+++ settings/asgi.py
@@ -1,10 +1,14 @@
 import os
 from django.core.asgi import get_asgi_application
-from interface.socket import app as fastapi_app  # Import FastAPI app
+from interface.socket import app as fastapi_app
+from starlette.staticfiles import StaticFiles
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 
 django_application = get_asgi_application()
+
+# Serve static files
+fastapi_app.mount("/static", StaticFiles(directory="staticfiles"), name="static")
 
 
 class CombinedApplication:
@@ -17,5 +21,6 @@ class CombinedApplication:
             await self.fastapi_app(scope, receive, send)
         else:
             await self.django_app(scope, receive, send)
+
 
 application = CombinedApplication(django_application, fastapi_app)
--- settings/settings.py
+++ settings/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import decouple
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collected static files
+
+# Enable Whitenoise for static file compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import django_heroku
+django_heroku.settings(locals())
