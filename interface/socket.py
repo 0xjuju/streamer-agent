c1ced748cd438f99ecaa83beb51af3ef1625d856
0xjuju <jermol.jupiter@gmail.com>
2025-01-27 21:03:21 -0500

    .

--- interface/socket.py
+++ interface/socket.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from framework.agents import OpenAI
+from text_to_speech.azure_speech_api import tts
 
 app = FastAPI()
 openai_api = OpenAI()
@@ -12,11 +13,11 @@ async def chat_endpoint(request: Request):
 
     data = await request.json()
     user_message = data.get("message")
-
     messages = openai_api.process_message(message=user_message, instructions="", thread_id="thread_ZhdVMfBTRXm7orSa9Rn0iSwd")
     reply = next((message.content[0].text for message in messages if message.role == "assistant"), None).value
-
-    return JSONResponse({"reply": f"Tony: {reply}"})
+    tts(reply)
+    response_text = f"Tony: {reply}"
+    return JSONResponse({"reply": response_text})
 
 
 
--- text_to_speech/azure_speech_api.py
+++ text_to_speech/azure_speech_api.py
@@ -2,39 +2,28 @@
 import azure.cognitiveservices.speech as speechsdk
 import decouple
 
-
 speech_config = speechsdk.SpeechConfig(subscription=decouple.config('AZURE_SPEECH_KEY1'),
-                                       region=decouple.config('AZURE_SPEECH_REGION'))
+                                           region=decouple.config('AZURE_SPEECH_REGION'))
 
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 
-speech_config.speech_synthesis_voice_name='en-US-DustinMultilingualNeural'
+speech_config.speech_synthesis_voice_name = 'en-US-DustinMultilingualNeural'
 
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-text = """
-<speak xmlns="http://www.w3.org/2001/10/synthesis" version="1.0" xml:lang="en-US" xmlns:mstts="https://www.w3.org/2001/mstts">
-  <voice name="en-US-DustinMultilingualNeural"> 
-    WOW!! That's a good dad joke!!
-    <mstts:express-as style="cheerful">Ha ha ha! That's so funny!</mstts:express-as>
-    
-     
-  </voice>
-</speak>
-"""
+def tts(text: str):
 
-# <prosody pitch="-15%" rate="55%" volume="+20%">Look how they massacred my boy!</prosody>
-# <prosody pitch="+20%" rate="fast" volume="+85%">Ha ha ha! hee hee heeheeheeheeeeeeee That’s hilarious!</prosody>
-#     <prosody pitch="+10%" rate="medium">Hee hee hee... Oh, stop it!</prosody>
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-speech_synthesis_result = speech_synthesizer.speak_ssml_async(text).get()
+    speech_synthesizer.speak_text_async(text).get()
 
-if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-    print("Speech synthesized for text [{}]".format(text))
-elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
-    cancellation_details = speech_synthesis_result.cancellation_details
-    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
-    if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        if cancellation_details.error_details:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
+    # if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    #     print("Speech synthesized for text [{}]".format(text))
+    # elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+    #     cancellation_details = speech_synthesis_result.cancellation_details
+    #     print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    #     if cancellation_details.reason == speechsdk.CancellationReason.Error:
+    #         if cancellation_details.error_details:
+    #             print("Error details: {}".format(cancellation_details.error_details))
+    #             print("Did you set the speech resource key and region values?")
+
+
